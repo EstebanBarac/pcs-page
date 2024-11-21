b0e326f4548f4d0eec42a421e3107b9350a61761
Esteban Barac <EstebanBarac8@gmail.com>
2024-11-21 10:31:36 -0300

    ig link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Instagram, MessageCircle } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Footer() {
@@ -14,14 +15,32 @@ export default function Footer() {
             <ul className="space-y-2">
             <li><Link href="/" className="text-gray-400 hover:text-white">Inicio</Link></li>
               <li><Link href="/categoria/pcs-nuevas-graficas-outlet" className="text-gray-400 hover:text-white">PCs Nuevas con Gráficas Outlet</Link></li>
-              <li><Link href="/categoria/pcs-nuevas-rtx-40" className="text-gray-400 hover:text-white">PCs Nuevas con Placas Nuevas</Link></li>
+              <li><Link href="/categoria/pcs-nuevas-rtx-40" className="text-gray-400 hover:text-white">PCs Nuevas</Link></li>
               <li><Link href="/categoria/placas-de-video" className="text-gray-400 hover:text-white">Placas de Video</Link></li>
               <li><Link href="/categoria/perifericos" className="text-gray-400 hover:text-white">Periféricos</Link></li>
+              <li><Link href="/categoria/perifericos" className="text-gray-400 hover:text-white">¿Qué PC Elegir?</Link></li>
             </ul>
           </div>
           <div>
-            <h3 className="text-lg font-semibold mb-4">Contactanos</h3>
-            <p className="text-gray-400">WhatsApp: (+54) 9 299-5286-048</p>
+            <h3 className="text-lg font-semibold mb-2">Contactanos</h3>
+            <Link 
+              href="https://wa.me/5492995286048" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="flex items-center text-gray-400  hover:text-white transition-colors"
+            >
+              <MessageCircle className="w-5 h-5 mr-2" />
+              <span>WhatsApp: 2995286048</span>
+            </Link>
+            <Link 
+              href="https://www.instagram.com/noxuspc_/profilecard/?igsh=NDdxY3NuaDB4NHVi" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="flex mt-4 items-center text-lg text-gray-400 hover:text-white transition-colors"
+            >
+              <Instagram className="w-5 h-5 mr-2" />
+              <span>@noxuspc_</span>
+            </Link>
           </div>
         </div>
       </div>
